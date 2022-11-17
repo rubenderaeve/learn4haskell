1ffc20f0d745e6a7dda06eb448d12980e81379cd
Ruben De Raeve <91075917+rubenderaeve@users.noreply.github.com>
2022-11-17 15:39:47 +0100

    chapter two: task nine

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -774,7 +774,8 @@ the list with only those lists that contain a passed element.
 
 🕯 HINT: Use the 'elem' function to check whether an element belongs to a list
 -}
-contains = error "contains: Not implemented!"
+contains :: Int -> [[Int]] -> [[Int]]
+contains el l = filter (elem el) l
 
 
 {- |
